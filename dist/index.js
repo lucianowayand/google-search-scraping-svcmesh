5e4bcb723ed0ea546f163535325e1ceffe567b56
Luciano <36968613+lucianowayand@users.noreply.github.com>
2023-11-30 09:18:27 -0300

    Revert "fix: vercel deployment"
    This reverts commit e166d370aa6341943bdd9e84c2333e1da904fcb9.

---
+++ dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const app = (0, express_1.default)();
+const port = process.env.PORT || 8080;
+const prisma = new client_1.PrismaClient();
+app.get("/prisma", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield prisma.user.findMany();
+    return res.json(users);
+}));
+app.get("/", (_req, res) => {
+    return res.json({ version: process.env.npm_package_version });
+});
+app.get("/hello", (_req, res) => {
+    return res.json({ message: "Hello world!" });
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on http://localhost:${port}`);
+});
+//# sourceMappingURL=index.js.map
