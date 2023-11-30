7a24c4fa35f319d713a2cafe3bb410b35eb9128f
Luciano <36968613+lucianowayand@users.noreply.github.com>
2023-11-30 08:45:34 -0300

    feat!: prisma config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const prisma = new PrismaClient();
+
+app.get("/prisma", async (_req: Request, res: Response) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
+});
 
 app.get("/", (_req: Request, res: Response) => {
   return res.json({ version: process.env.npm_package_version });
 });
 
+app.get("/hello", (_req: Request, res: Response) => {
+  return res.json({ message: "Hello world!" });
+});
+
 app.listen(port, () => {
-  return console.log(`Server is listening on http://http://localhost:${port}`);
+  return console.log(`Server is listening on http://localhost:${port}`);
 });
